6542553765bd949e32e024d9445c6b3fc4d92f2e
rafaeldeoliveiracosta <rafaeldeoliveiracosta@9cf0c52f-d1ff-5b1a-c6ba-16225875fc5f>
2010-04-15 02:26:32 +0000

    /*  * Arquivo onde e feita a implementacao das funcoes da camada fisica  *  * Componentes: Felipe Almeida Tavares  *              Leandro Lima Monteiro Coelho  *		Rafael de Oliveira Costa  * /

---
+++ trunk/fisica-test.c
@@ -0,0 +1,9 @@
+/*
+ * Arquivo contendo uma aplicacao para teste das funcoes da camada fisica
+ *
+ * Componentes: Felipe Almeida Tavares
+ *              Leandro Lima Monteiro Coelho
+ *		Rafael de Oliveira Costa
+ *
+/
+
---
+++ trunk/fisica.h
@@ -0,0 +1,10 @@
+/*
+ * Arquivo que contem as definicoes de estruturas de dados e das assinaturas das
+ * funcoes publicas da camada fisica
+ *
+ * Componentes: Felipe Almeida Tavares
+ *              Leandro Lima Monteiro Coelho
+ *		Rafael de Oliveira Costa
+ *
+/
+
