7b0440c16118a988c0d8bfd75f87c4b4e5696e7a
rafaeldeoliveiracosta <rafaeldeoliveiracosta@9cf0c52f-d1ff-5b1a-c6ba-16225875fc5f>
2010-04-17 17:13:23 +0000

    Primeira Funcao Funcionando

--- fisica.c
+++ fisica.c
@@ -10,14 +10,34 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h> 
+#include <stdlib.h>
+
+int sockfd;
+struct sockaddr_in addr;
 
 int P_Activate_Request(int port, char *end){
-
-	int sockfd;
 	
-	sockfd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if ((sockfd = socket(PF_UNIX, SOCK_STREAM, 0)) < 0)
+	{
+	   printf("--Erro na criacao do socket\n");
+	   return 0;
+	}
 
-	//teste
+	memset(&addr, 0, sizeof(addr));
+  	addr.sin_family = AF_UNIX;
+  	addr.sin_addr.s_addr = inet_addr(end);
+  	addr.sin_port = htons(port);
+
+	if (bind(sockfd,(struct sockaddr *)&addr, sizeof(struct sockaddr)) < 0) {
+    	   printf("--Exit com erro no bind \n");
+    	   close(sockfd);
+    	   return 0;
+  	} 
+
+	return 1;
 
 }
-
--- makefile
+++ makefile
@@ -1,4 +1,5 @@
 fisica: fisica.c fisica.h
-	gcc -o fisica fisica.c fisica.h 
+	gcc -o fisica fisica-test.c fisica.c fisica.h 
 clean: fisica
 	rm fisica
+
