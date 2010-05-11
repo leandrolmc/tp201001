39130e735893193d0b7fdb179e765090bb2de4b4
rafaeldeoliveiracosta <rafaeldeoliveiracosta@9cf0c52f-d1ff-5b1a-c6ba-16225875fc5f>
2010-05-11 12:38:08 +0000

    Arquivos de enlace

---
+++ trunk/enlace-test.c
@@ -0,0 +1,14 @@
+/*
+ * Arquivo contendo uma aplicacao para teste das funcoes da camada de enlace
+ *
+ * Componentes: Felipe Almeida Tavares
+ *              Leandro Lima Monteiro Coelho
+ *		Rafael de Oliveira Costa
+ */
+
+#include "enlace.h"
+
+int main(){
+
+   return 0;
+}
---
+++ trunk/enlace.c
@@ -0,0 +1,11 @@
+/*
+ * Arquivo onde e feita a implementacao das funcoes da camada de enlace
+ *
+ * Componentes: Felipe Almeida Tavares
+ *              Leandro Lima Monteiro Coelho
+ *		Rafael de Oliveira Costa
+ */
+
+#include "enlace.h"
+
+
