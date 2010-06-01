b7ddbd8065b8796f9eb56aac355910ab00f34441
rafaeldeoliveiracosta <rafaeldeoliveiracosta@9cf0c52f-d1ff-5b1a-c6ba-16225875fc5f>
2010-06-01 03:21:29 +0000

    Makefile editado

--- enlace.c
+++ enlace.c
@@ -7,5 +7,19 @@
  */
 
 #include "enlace.h"
+#include <stdio.h>
 
+/* Efetua as inicializacoes do nivel de enlace (recebe o endereco local da maquina) e inicializa o nivel  
+ * fisico (recebe a especificacao da porta que sera usada para a comunicacao e o endereco IP do comutador de enlace)
+ * Faz o bind para a porta escolhida e faz as inicializacoes necessarias para a camada de enlace
+ * Retorna 1 em caso de sucesso e 0 em caso de falha
+ */
+
+int L_Activate_Request(unsigned char mac, int port, char *addr){
+	
+	if(P_Activate_Request(port, addr)){
+		printf("Conectou com a camada fisica\n");
+	}
+
+}
 
