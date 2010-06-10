72dea1ad5e7e0f1a58983ff017c0a2d5d1e2e932
rafaeldeoliveiracosta <rafaeldeoliveiracosta@9cf0c52f-d1ff-5b1a-c6ba-16225875fc5f>
2010-06-10 04:04:31 +0000

    Limpeza do codigo e problema na funcao unplug_host

--- trunk/comutador.h
+++ trunk/comutador.h
@@ -8,6 +8,13 @@
  */
 
 
+#define BUFFER_SIZE	128
+#define NUMBER_OF_PORTS 32
+
+//A porta do comutador deve ser padrao e todos os hosts devem ter conhecimento disso.
+#define SWITCH_PORT 	5000
+
+
 /* Efetua a inicializacao do comutador 
  * Retorna 1 em caso de sucesso e 0 em caso de falha
  */
@@ -16,12 +23,12 @@ int start_switch();
 /* Efetua as operacoes necessarias ao plugar um host no comutador 
  * Retorna 1 em caso de sucesso e 0 em caso de falha
  */
-int plug_host(unsigned char mac, int my_port, char *my_addr,int switch_port,char *switch_addr);
+int plug_host(unsigned char ,int , char * ,int ,char *);
 
 /* Efetua as operacoes necessarias ao desplugar um host no comutador 
  * Retorna 1 em caso de sucesso e 0 em caso de falha
  */
-int unplug_host(unsigned char mac);
+int unplug_host(unsigned char);
 
 
 /* Gera uma porta aleatoria para o comutador de acordo com o numero de enlaces do comutador
--- trunk/enlace.h
+++ trunk/enlace.h
@@ -8,31 +8,60 @@
  */
 
 /*
+ ***************************************
+ *** Estruturas de dados e constantes***
+ ***************************************
+ */
+
+/*
+ * Quadro de enlace: 
+ *    endereco de destino, 
+ *    endereco fonte, 
+ *    tamanho dos dados, 
+ *    dados, 
+ *    codigo de erro (escolher metodo para deteccao de erro).
+ * 
  * FRAME:          MACORIGEM|MACDESTINO|TAMANHODOSDADOS|DADOS|CODIGOERRO|
  *
  * FRAME ESPECIAL: MACORIGEM|000|PORTA|ENDERECOHOST|000|
  */
 
+/*
+ * Buffers para armazenar os quadros a serem transmitidos e quadros recebidos.
+ */
+
+/*
+ * Endereco broadcast (usar o valor 255)
+ */
+unsigned char broadcast=255; //endereco de broadcast
+
+/*
+ * Definir tamanho maximo de dados de um quadro.
+ *
+ */
+
+#define FRAME_SIZE 1536
+
+//////////////////////////////////////////
+
 #define PAYLOAD_SIZE 128	
+unsigned char my_mac; //endereco MAC do host que executar o software de enlace
 
 /* Efetua as inicializacoes do nivel de enlace (recebe o endereco local da maquina) e inicializa o nivel  
  * fisico (recebe a especificacao da porta que sera usada para a comunicacao e o endereco IP do comutador de enlace)
  * Faz o bind para a porta escolhida e faz as inicializacoes necessarias para a camada de enlace
  * Retorna 1 em caso de sucesso e 0 em caso de falha
  */
-
 int L_Activate_Request(unsigned char, int, char *);
 
 /* Solicita a transmissao de um quadro
  * Recebe o endereco MAC de destino, os dados a serem transmitidos e o numero de bytes
  */                        
-
 void L_Data_Request(unsigned char, char *, int);
 
 /* Testa se ha um quadro recebido no nivel de enlace
  * Retorna 1 caso exista e 0 caso contrario
  */
-
 int L_Data_Indication(void);
 
 /* Busca no nivel de enlace os dados do ultimo quadro recebido
@@ -45,7 +74,6 @@ int L_Data_Indication(void);
  * de falha (numero de bytes do quadro maior que o tamanho 
  * maximo esperado)
  */                                         
-
 int L_Data_Receive(unsigned char *, char *, int);
 
 /* Executa um ciclo de tarefas da camada de enlace e retorna
@@ -54,23 +82,19 @@ int L_Data_Receive(unsigned char *, char *, int);
  * Deve verificar se ha ́um byte para receber no nivel fisico e,
  * caso exista, receber esse byte
  */
-
 void L_MainLoop(void);
 
 /* Configura a taxa de perda de quadros da camada de enlace
  * Recebe o valor percentual de perda de quadros
  */
-
 void L_Set_Loss_Probability(float);
 
 // Finaliza o funcionamento do nivel de enlace e do nivel fısico
-
 void L_Deactivate_Request(void);
 
 /* Recebe um byte e armazena no buffer da camada de enlace,
  * quando o ultimo byte de um quadro e recebido ele deve ser validado
  */
-
 void l_Recebe_Byte(void); 
 
 /* Valida um quadro recebido, apenas mantem um quadro no buffer de recepcao se for
@@ -78,10 +102,8 @@ void l_Recebe_Byte(void);
  * checagem de erro (usar probabilidade randomica de erro para
  * “forcar” a ocorrencia de erros)
  */
-
 void l_Valida_Quadro(void); 
 
-// Transmite um byte do quadro e checa se terminou a transmissao do quadro
-                               
+// Transmite um byte do quadro e checa se terminou a transmissao do quadro                            
 void l_Transmite_Byte(void);
 
