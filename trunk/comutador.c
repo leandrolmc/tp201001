2e4a12ba1c6714b8749487a684038706a38bd6ec
fe.atavares <fe.atavares@9cf0c52f-d1ff-5b1a-c6ba-16225875fc5f>
2010-06-17 22:02:28 +0000

    ta indo...

--- trunk/comutador.c
+++ trunk/comutador.c
@@ -19,8 +19,8 @@
 
 //Estrutura que representa a tabela de emulacao das conexoes fisicas com o comutador
 struct table_phy {
-	int port_switch;
-	char *mac;
+//	int port_switch;
+	unsigned char mac;
 	int port;
 	char *address;
 };
@@ -31,14 +31,21 @@ struct table_switch {
 	char *mac;
 };
 
+int last_used_port = -1; // portas fisicas (sequencial). -1 = nenhuma usada.
+
 //Declarando as tabelas como variaveis globais
 struct table_phy table_phy[NUMBER_OF_PORTS];
 struct table_switch table_switch[NUMBER_OF_PORTS];
 
 struct sockaddr_in local_addr; // informacoes de endereco local
 int socket_conexoes; // socket responsavel por aguardar as conexoes fisicas
+int socket_comunicacao; // socket responsavel pela comunicacao
+struct pollfd ufds_con[1]; //pollfd para conexoes fisicas
+struct pollfd ufds_comm[1]; //pollfd para comunicacao
+
 char buffer_recv[BUFFER_SIZE]; //buffer onde os bytes recebidos serão armazenados
-struct pollfd ufds[1]; //Contains information used to poll the status of a transport endpoint
+char buffer_env[BUFFER_SIZE]; //buffer onde os bytes enviados serão armazenados
+
 
 
 void init(void) {
@@ -50,8 +57,12 @@ void init(void) {
 		memset(&table_switch[i], 0, sizeof(table_switch[i]));
 	}
 
-	// criando o socket
+	// criando os sockets
         if ((socket_conexoes = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+           printf("--Erro na criacao do socket\n");
+           exit(-1);
+        }
+        if ((socket_comunicacao = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
            printf("--Erro na criacao do socket\n");
            exit(-1);
         }
@@ -69,18 +80,25 @@ void init(void) {
            exit(-1);
         }
 
-	ufds[0].fd = socket_conexoes;
-	ufds[0].events = POLLIN;
+	ufds_con[0].fd = socket_conexoes;
+	ufds_con[0].events = POLLIN;
+
+	ufds_comm[0].fd = socket_comunicacao;
+	ufds_comm[0].events = POLLIN;
 }
 
 void verifica_conexoes(void) {
-	int resultado = poll(ufds, 1, 1000);
+	int resultado = poll(ufds_con, 1, 1000);
 
 	if (resultado > 0) {
-		if (ufds[0].revents & POLLIN) {
+		if (ufds_con[0].revents & POLLIN) {
 			recvfrom(socket_conexoes, buffer_recv, sizeof(buffer_recv), 0, (struct sockaddr *)0, 0);
 		}
-		printf("Pacote recebido: %s\n\n", buffer_recv);
+		last_used_port++;
+
+		table_phy[last_used_port].address = strtok(buffer_recv, "|");
+		table_phy[last_used_port].port = atoi(strtok(NULL, "|"));
+		table_phy[last_used_port].mac = atoi(strtok(NULL, "|"));
 	}
 	else if (resultado == -1) {
 		printf("--erro no poll\n");
@@ -88,12 +106,16 @@ void verifica_conexoes(void) {
 	}
 }
 
+void recebe_frame(void) {
+	
+}
+
 int start_switch(){
 /*
 Loop:
 verifica conexoes
 recebe pacotes
-encaminha pacotes
+comuta pacotes
 envia pacotes
 
 */
@@ -101,6 +123,7 @@ envia pacotes
 
 	while (1) {
 		verifica_conexoes();
+		recebe_frame();
 	}
 
         return 1;
