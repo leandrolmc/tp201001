a274a0c3d145e8011c738454b88ee4bc55fb39b1
rafaeldeoliveiracosta <rafaeldeoliveiracosta@9cf0c52f-d1ff-5b1a-c6ba-16225875fc5f>
2010-07-17 21:29:25 +0000

    arquivos backbone criados

---
+++ backbone.h
@@ -0,0 +1,11 @@
+/*
+ * Arquivo que contem as definicoes de estruturas de dados e das assinaturas das
+ * funcoes publicas do backbone
+ *
+ * Componentes: Felipe Almeida Tavares
+ *              Leandro Lima Monteiro Coelho
+ *					 Rafael de Oliveira Costa
+ */
+
+
+
