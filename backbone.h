cb0c17238858121bb829363925ea6659fa61373e
rafaeldeoliveiracosta <rafaeldeoliveiracosta@9cf0c52f-d1ff-5b1a-c6ba-16225875fc5f>
2010-07-19 02:12:43 +0000

    quebrando a cabeça com sockets e poll

--- backbone.h
+++ backbone.h
@@ -8,18 +8,23 @@
  */
 
 
+#define NUMBER_OF_INTERFACES	4
 #define TABLE_LENGTH 			16
+#define BACKBONE_IP				"127.0.0.1"
 #define BACKBONE_PORT 			5000
 #define BUFFER_SIZE				1024
-
+#define DATAGRAMA_SIZE			1024
 
 /* Efetua a inicializacao do backbone
  * Retorna 1 em caso de sucesso e 0 em caso de falha
  */
 int start_backbone();
 
-//
+// Funcao Encarregada de fazer as inicializações das variaveis do backbone
 void init();
+
+// Funcao Encarregada de fazer a verificacao de conexoes logicas vindas de roteadores de borda
+void verifica_conexoes(void);
 
 /* Adiciona uma rota na tabela de redirecionamento do backbone
  * Retorna 1 em caso de sucesso e 0 em caso de falha
@@ -33,3 +38,6 @@ int route_del(char *,char *);
 
 // Exibe uma lista com todas as entradas da tabela de redirecionamento que estão ocupadas
 void list_table();
+
+// Efetua o redirecionamento dos datagramas para a interface correta
+void rotear_pacotes();
